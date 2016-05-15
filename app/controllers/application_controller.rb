45d9da7a83e8413579cb99b986c09a6c3edca15e
Ian Lloyd <ian.w.lloyd@gmail.com>
2016-05-15 19:11:13 +0100

    Allow current_user in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html, :json
 
+  helper_method :current_user
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
